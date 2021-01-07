c767adf8dd916dd70b8b54b0ea51ba492d4a5313
FelixFeuerigel <felix.feuerigel@gmail.com>
2021-01-07 03:08:08 +0100

    corrected output of date and time in rss-feed

--- scripts/rss_parser.js
+++ scripts/rss_parser.js
@@ -8,13 +8,12 @@ feednami.load(url)
     for(let entry of feed.entries){
         //create a list element
         let li = document.createElement('li');
-        let date = new Date(entry.pubdate);
         //add HTML content to list items
+        let date = new Date(entry.pubdate);
         li.innerHTML = `
-        <time>${date.getDate()}.${date.getMonth()}.${date.getFullYear()}, ${date.getHours()}:${date.getMinutes()} </time>
         <h4><a href="${feed.meta.link}">${feed.meta.title}</a></h4>
-        <h5><a href="${entry.link}">${entry.title}</a></h5>
-        <div> ${entry.description} </div>
+        <div><a href="${entry.link}">${entry.title}</a> <br> <cite> ${entry.description}</cite></div>
+        <time>${date.toLocaleString()}</time>
         `;
         //append HTML content to list 
         textarea.appendChild(li);
