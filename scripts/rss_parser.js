06a0185fbe43261e1ffddd24bdfdb72afe181440
FelixFeuerigel <felix.feuerigel@gmail.com>
2021-01-07 02:39:03 +0100

    fixing day output in RSS-feed

--- scripts/rss_parser.js
+++ scripts/rss_parser.js
@@ -3,15 +3,15 @@ const textarea = document.querySelector('#feed-textarea > ul');
 
 feednami.load(url)
 .then(feed => {
-    textarea.value = ''
+    textarea.value = '';
     console.log(feed);
     for(let entry of feed.entries){
         //create a list element
         let li = document.createElement('li');
-        let date = new Date(entry.pubdate)
+        let date = new Date(entry.pubdate);
         //add HTML content to list items
         li.innerHTML = `
-        <time>${date.getDay()}.${date.getMonth()}.${date.getFullYear()}, ${date.getHours()}:${date.getMinutes()} </time>
+        <time>${date.getDate()}.${date.getMonth()}.${date.getFullYear()}, ${date.getHours()}:${date.getMinutes()} </time>
         <h4><a href="${feed.meta.link}">${feed.meta.title}</a></h4>
         <h5><a href="${entry.link}">${entry.title}</a></h5>
         <div> ${entry.description} </div>
