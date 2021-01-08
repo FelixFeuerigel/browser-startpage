39c69a7d414af4d58a99106d4934b2bd9d425d66
FelixFeuerigel <felix.feuerigel@gmail.com>
2021-01-08 13:39:39 +0100

    bigger links in buttons

--- scripts/rss_parser.js
+++ scripts/rss_parser.js
@@ -1,8 +1,7 @@
 const textarea = document.querySelector('#feed-textarea > ul');
 const adresses = [
-    'https://www.youtube.com/feeds/videos.xml?channel_id=UCsXVk37bltHxD1rDPwtNM8Q',
-    'https://www.youtube.com/feeds/videos.xml?channel_id=UCBJycsmduvYEL83R_U4JriQ'
-    
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UCSbdMXOI_3HGiFviLZO6kNA", //ThrillSeeker
+    "https://www.youtube.com/feeds/videos.xml?channel_id=UCSbdMXOI_3HGiFviLZO6kNA", //TechLinked
 ];
 
 async function getFeed(url = ""){
@@ -34,14 +33,14 @@ async function printFeed(adresses = []){
         let li = document.createElement('li');
         //add HTML content to list items
         li.innerHTML = `
-        <h4><a href="${feedEntry.meta.link}">${feedEntry.meta.title}</a></h4>
-        <div><a href="${feedEntry.entry.link}">${feedEntry.entry.title}</a>
+        <h4 class="feed-title"><a href="${feedEntry.meta.link}">${feedEntry.meta.title}</a></h4>
+        <h5 class="feed-title2"><a href="${feedEntry.entry.link}">${feedEntry.entry.title}</a></h5>
         `;
         if(feedEntry.entry.description != null){ 
             li.innerHTML += ` <br> <cite> ${feedEntry.entry.description}</cite>`;
         }
-        li.innerHTML += `</div>
-        <time>${feedEntry.date.toLocaleString()}</time>
+        li.innerHTML += `
+        <time  class="feed-time">${feedEntry.date.toLocaleString()}</time>
         `;
         //append HTML content to list 
         textarea.appendChild(li);
