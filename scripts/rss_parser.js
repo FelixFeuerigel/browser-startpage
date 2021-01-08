c082b8cef6a24b57f30abaecf44ace12d1279d14
FelixFeuerigel <felix.feuerigel@gmail.com>
2021-01-08 13:06:17 +0100

    combining and sorting of multiple rss feeds

--- scripts/rss_parser.js
+++ scripts/rss_parser.js
@@ -1,21 +1,51 @@
-let url = 'https://myanimelist.net/rss.php?type=rwe&u=DasIchigo';
 const textarea = document.querySelector('#feed-textarea > ul');
+const adresses = [
+    'https://www.youtube.com/feeds/videos.xml?channel_id=UCsXVk37bltHxD1rDPwtNM8Q',
+    'https://www.youtube.com/feeds/videos.xml?channel_id=UCBJycsmduvYEL83R_U4JriQ'
+    
+];
 
-feednami.load(url)
-.then(feed => {
-    textarea.value = '';
-    console.log(feed);
-    for(let entry of feed.entries){
+async function getFeed(url = ""){
+    console.log(`Loading feed from: ${url}`);
+    return feednami.load(url).then(feed => {
+        let orderedFeed = [];
+        console.log(feed);
+        console.log(`Success getting feed from: ${url}`);
+        for(let entry of feed.entries){
+
+            orderedFeed.push({
+                meta: feed.meta,
+                date: new Date(entry.pubdate),
+                entry: entry
+            });
+        }
+        return orderedFeed;
+    });
+}
+async function printFeed(adresses = []){
+    let combinedFeed = [];
+    for(url of adresses){
+        let newFeed = await getFeed(url);
+        combinedFeed = [...combinedFeed, ...newFeed]
+    }
+    combinedFeed.sort( (a, b) => b.date - a.date );
+    for(feedEntry of combinedFeed){
         //create a list element
         let li = document.createElement('li');
         //add HTML content to list items
-        let date = new Date(entry.pubdate);
         li.innerHTML = `
-        <h4><a href="${feed.meta.link}">${feed.meta.title}</a></h4>
-        <div><a href="${entry.link}">${entry.title}</a> <br> <cite> ${entry.description}</cite></div>
-        <time>${date.toLocaleString()}</time>
+        <h4><a href="${feedEntry.meta.link}">${feedEntry.meta.title}</a></h4>
+        <div><a href="${feedEntry.entry.link}">${feedEntry.entry.title}</a>
+        `;
+        if(feedEntry.entry.description != null){ 
+            li.innerHTML += ` <br> <cite> ${feedEntry.entry.description}</cite>`;
+        }
+        li.innerHTML += `</div>
+        <time>${feedEntry.date.toLocaleString()}</time>
         `;
         //append HTML content to list 
         textarea.appendChild(li);
     }
-});
+}
+
+printFeed(adresses);
